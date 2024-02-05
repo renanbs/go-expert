3877cba5ebd22fa2c26d26ad39f1809a1b108cfb
Renan Biegelmeyer <renan.biegel@gmail.com>
2024-02-04 22:47:37 -0300

    pointers

---
+++ 15-ponteiros/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	a := 10
+	println(a)
+	println(&a)
+	var pointer *int = &a
+	println(pointer)
+	println(*pointer)
+	*pointer = 20
+	println(a)
+	b := &a
+	println(b)
+	println(*b)
+	*b = 30
+	println(*b)
+}
